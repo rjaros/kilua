d842cbf56fe31777d059374fdc13c5ef25b74675
Robert Jaros <rjaros@finn.pl>
2023-11-07 07:21:47 +0100

    Apply binaryen to wasm

--- examples/bootstrap-form/build.gradle.kts
+++ examples/bootstrap-form/build.gradle.kts
@@ -47,6 +47,20 @@ kotlin {
                 }
             }
             binaries.executable()
+            applyBinaryen {
+                binaryenArgs = mutableListOf(
+                    "--enable-nontrapping-float-to-int",
+                    "--enable-gc",
+                    "--enable-reference-types",
+                    "--enable-exception-handling",
+                    "--enable-bulk-memory",
+                    "--inline-functions-with-loops",
+                    "--traps-never-happen",
+                    "--fast-math",
+                    "-O1",
+                    "-c" // Run passes while binary size decreases
+                )
+            }
         }
     }
     sourceSets {
