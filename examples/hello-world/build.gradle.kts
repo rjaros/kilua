85447577ac426b2ab540b7d5d16c84b5785ea02c
Robert Jaros <rjaros@finn.pl>
2023-11-07 07:22:10 +0100

    Hello world example

---
+++ examples/hello-world/build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.compose")
+}
+
+val isInIdea = System.getProperty("idea.vendor.name") != null
+val buildTarget: String by project
+
+@OptIn(ExperimentalWasmDsl::class)
+kotlin {
+    if (buildTarget == "js" || !isInIdea) {
+        js(IR) {
+            useEsModules()
+            browser {
+                commonWebpackConfig {
+                    outputFileName = "main.bundle.js"
+                }
+                runTask {
+                    sourceMaps = false
+                }
+                testTask {
+                    useKarma {
+                        useChromeHeadless()
+                    }
+                }
+            }
+            binaries.executable()
+        }
+    }
+    if (buildTarget == "wasm" || !isInIdea) {
+        wasmJs {
+            useEsModules()
+            browser {
+                commonWebpackConfig {
+                    outputFileName = "main.bundle.js"
+                }
+                runTask {
+                    sourceMaps = false
+                }
+                testTask {
+                    useKarma {
+                        useChromeHeadlessWasmGc()
+                    }
+                }
+            }
+            binaries.executable()
+            applyBinaryen {
+                binaryenArgs = mutableListOf(
+                    "--enable-nontrapping-float-to-int",
+                    "--enable-gc",
+                    "--enable-reference-types",
+                    "--enable-exception-handling",
+                    "--enable-bulk-memory",
+                    "--inline-functions-with-loops",
+                    "--traps-never-happen",
+                    "--fast-math",
+                    "-O1",
+                    "-c" // Run passes while binary size decreases
+                )
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(project(":kilua"))
+            }
+        }
+        if (buildTarget == "js" || !isInIdea) {
+            val jsMain by getting {
+                dependencies {
+                }
+            }
+        }
+        if (buildTarget == "wasm" || !isInIdea) {
+            val wasmJsMain by getting {
+                dependencies {
+                }
+            }
+        }
+    }
+}
+
+compose {
+    kotlinCompilerPlugin.set(libs.versions.compose.plugin)
+//    kotlinCompilerPluginArgs.add("suppressKotlinVersionCompatibilityCheck=${libs.versions.kotlin.get()}")
+}
