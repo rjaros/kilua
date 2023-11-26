23a5b2ce4db4a721803e3e6432a098c78e83957c
Robert Jaros <rjaros@finn.pl>
2023-11-26 16:56:02 +0100

    New version of kilua-assets

--- kilua-assets/build.gradle.kts
+++ kilua-assets/build.gradle.kts
@@ -12,7 +12,7 @@ npmPublish {
             }
             packageJson {
                 main.set("index.js")
-                version.set("0.0.5")
+                version.set("0.0.8")
                 description.set("The assets for the Kilua framework")
                 keywords.set(listOf("kilua", "kotlin", "wasm"))
                 homepage.set("https://kilua.dev")
