4dfe6ae927e333777ab870390a22758d110b964a
Robert Jaros <rjaros@finn.pl>
2024-07-04 15:40:30 +0200

    Upgraded dependencies (Kilua RPC 0.0.13, Atomicfu 0.25.0, Serialization 1.7.1, Jooby 3.2.3, Ktor 2.3.12, Spring Boot 3.3.1, kilua-assets 0.0.12, marked 13.0.2, postcss 8.4.39, cssnano 7.0.3)

--- kilua-assets/build.gradle.kts
+++ kilua-assets/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val packageJsonFun: PackageJson.() -> Unit = {
     main.set("index.js")
-    version.set("0.0.11")
+    version.set("0.0.12")
     description.set("The assets for the Kilua framework")
     keywords.set(listOf("kilua", "kotlin", "wasm"))
     homepage.set("https://kilua.dev")
